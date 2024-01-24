6fbf7d1c27dee8f9c743c009daaa548cd2423b8e
jason@mxcloud.services <Jason@mxcloud.services>
2024-01-24 10:57:35 -0500

    Create Lock-MosyleDevice.ps1 Restart-MosyleDevice

--- Source/Public/Restart-MosyleDevice.ps1
+++ Source/Public/Restart-MosyleDevice.ps1
@@ -1,20 +1,15 @@
-function Lock-MosyleDevice {
+function Restart-MosyleDevice {
   param(
     [parameter(Mandatory, Position=0)]
-    [string]$Identity,
-    [parameter(Mandatory, Position=1)]
-    [ValidatePattern('[0-9]{6}')]
-    [int32]$PinCode,
-    [string]$LockMessage
+    [string]$Identity
   )
   $device = Get-MosyleDevice -Identity $Identity -DeviceIDOnly
 
   $mosyleAPI                   = [hashtable]::new()
   $mosyleAPI.Body              = [hashtable]::new()
-  $mosyleAPI.Body.pincode      = $PinCode
   $mosyleAPI.Body.devices      = $device
-  $mosyleAPI.Body.operation    = 'lock_device'
-  $mosyleAPI.Body.lockmessage  = $lockMessage
+  $mosyleAPI.Body.operation    = 'restart_devices'
+  $mosyleAPI.Body.lockmessage  = $message
   $mosyleAPI.Method            = 'POST'
   $mosyleAPI.Endpoint          = 'devices'
   Invoke-MosyleAPI @mosyleAPI
